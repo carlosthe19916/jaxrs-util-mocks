1bdb0583ffe8ce56e1d1bf84333b9ec0a7259369
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-05-09 15:18:05 +0200

    added report mocks

---
+++ src/main/java/org/acme/quickstart/ReportResource.java
@@ -0,0 +1,115 @@
+package org.acme.quickstart;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Path("/api/xavier/camel/report")
+public class ReportResource {
+
+    public static final List<Report> REPORTS = new ArrayList<>();
+    public static final Map<Long, Report> REPORTS_MAP = new HashMap<>();
+
+    static {
+        Report report1 = new Report(1L, "36500629", "2019-05-09-36500629-payload.json", 61568L, 712997328L, 2585856L);
+        Report report2 = new Report(2L, "24353269", "2019-05-09-24353269-payload.json", 43260L, 204442917L, 1816920L);
+        Report report3 = new Report(3L, "5705978", "2019-05-09-5705978-payload.json", 68458L, 906625456L, 2875236L);
+
+        REPORTS.add(report1);
+        REPORTS.add(report2);
+        REPORTS.add(report3);
+
+        REPORTS_MAP.put(report1.getId(), report1);
+        REPORTS_MAP.put(report2.getId(), report2);
+        REPORTS_MAP.put(report3.getId(), report3);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Report> getAllReports() {
+        return REPORTS;
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Report getReportById(@PathParam("id") Long id) {
+        return REPORTS_MAP.get(id);
+    }
+
+    public static class Report {
+        private Long id;
+        private String customerId;
+        private String fileName;
+        private Long numberOfHosts;
+        private Long totalDiskSpace;
+        private Long totalPrice;
+
+        public Report() {
+
+        }
+
+        public Report(Long id, String customerId, String fileName, Long numberOfHosts, Long totalDiskSpace, Long totalPrice) {
+            this.id = id;
+            this.customerId = customerId;
+            this.fileName = fileName;
+            this.numberOfHosts = numberOfHosts;
+            this.totalDiskSpace = totalDiskSpace;
+            this.totalPrice = totalPrice;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getCustomerId() {
+            return customerId;
+        }
+
+        public void setCustomerId(String customerId) {
+            this.customerId = customerId;
+        }
+
+        public String getFileName() {
+            return fileName;
+        }
+
+        public void setFileName(String fileName) {
+            this.fileName = fileName;
+        }
+
+        public Long getNumberOfHosts() {
+            return numberOfHosts;
+        }
+
+        public void setNumberOfHosts(Long numberOfHosts) {
+            this.numberOfHosts = numberOfHosts;
+        }
+
+        public Long getTotalDiskSpace() {
+            return totalDiskSpace;
+        }
+
+        public void setTotalDiskSpace(Long totalDiskSpace) {
+            this.totalDiskSpace = totalDiskSpace;
+        }
+
+        public Long getTotalPrice() {
+            return totalPrice;
+        }
+
+        public void setTotalPrice(Long totalPrice) {
+            this.totalPrice = totalPrice;
+        }
+    }
+}
