9ffed71fa356d1560e96e294dfb9d0157f40b19c
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-05-09 17:11:36 +0200

    added report mocks

--- src/main/java/org/acme/quickstart/ReportResource.java
+++ src/main/java/org/acme/quickstart/ReportResource.java
@@ -5,10 +5,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Path("/api/xavier/camel/report")
 public class ReportResource {
@@ -17,17 +14,21 @@ public class ReportResource {
     public static final Map<Long, Report> REPORTS_MAP = new HashMap<>();
 
     static {
-        Report report1 = new Report(1L, "36500629", "2019-05-09-36500629-payload.json", 61568L, 712997328L, 2585856L);
-        Report report2 = new Report(2L, "24353269", "2019-05-09-24353269-payload.json", 43260L, 204442917L, 1816920L);
-        Report report3 = new Report(3L, "5705978", "2019-05-09-5705978-payload.json", 68458L, 906625456L, 2875236L);
-
+        Report report1 = new Report(1L, "36500629", "2019-05-09-36500629-payload.json", 61568L, 712997328L, 2585856L, new Date().getTime());
         REPORTS.add(report1);
-        REPORTS.add(report2);
-        REPORTS.add(report3);
-
         REPORTS_MAP.put(report1.getId(), report1);
+
+        Report report2 = new Report(2L, "24353269", "2019-05-09-24353269-payload.json", 43260L, 204442917L, 1816920L, new Date().getTime());
+        REPORTS.add(report2);
         REPORTS_MAP.put(report2.getId(), report2);
+
+        Report report3 = new Report(3L, "5705978", "2019-05-09-5705978-payload.json", 68458L, 906625456L, 2875236L, new Date().getTime());
+        REPORTS.add(report3);
         REPORTS_MAP.put(report3.getId(), report3);
+
+        Report report4 = new Report(4L, "5705978", "2019-05-09-5705978-payload.json", 68458L, 906625456L, 2875236L, new Date().getTime());
+        REPORTS.add(report4);
+        REPORTS_MAP.put(report4.getId(), report4);
     }
 
     @GET
@@ -50,18 +51,20 @@ public class ReportResource {
         private Long numberOfHosts;
         private Long totalDiskSpace;
         private Long totalPrice;
+        private Long creationDate;
 
         public Report() {
 
         }
 
-        public Report(Long id, String customerId, String fileName, Long numberOfHosts, Long totalDiskSpace, Long totalPrice) {
+        public Report(Long id, String customerId, String fileName, Long numberOfHosts, Long totalDiskSpace, Long totalPrice, Long creationDate) {
             this.id = id;
             this.customerId = customerId;
             this.fileName = fileName;
             this.numberOfHosts = numberOfHosts;
             this.totalDiskSpace = totalDiskSpace;
             this.totalPrice = totalPrice;
+            this.creationDate = creationDate;
         }
 
         public Long getId() {
@@ -110,6 +113,14 @@ public class ReportResource {
 
         public void setTotalPrice(Long totalPrice) {
             this.totalPrice = totalPrice;
+        }
+
+        public Long getCreationDate() {
+            return creationDate;
+        }
+
+        public void setCreationDate(Long creationDate) {
+            this.creationDate = creationDate;
         }
     }
 }
